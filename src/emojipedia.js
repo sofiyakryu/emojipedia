e4cdbac020263727eaab8c3fea4b4faeabd45e4a
sofiyakryu <63737183+sofiyakryu@users.noreply.github.com>
2020-09-04 15:28:09 +0200

    Mapping Data to Components
    dictionary tags: dl, dt, dd;
    mapping components;
    extracting components;

---
+++ src/emojipedia.js
@@ -0,0 +1,46 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🥰",
+    name: "Smiling Face with Hearts",
+    meaning:
+      "A yellow face with smiling eyes, a closed smile, rosy cheeks, and several hearts floating around its head. Expresses a range of happy, affectionate feelings, especially being in love. The number of hearts (three or more) varies by platform."
+  },
+  {
+    id: 5,
+    emoji: "🤩",
+    name: "Star - Struck",
+    meaning:
+      "A yellow face with a broad, open smile, showing upper teeth on most platforms, with stars for eyes, as if seeing a beloved celebrity. Color of stars widely varies by platform, though commonly gold or red. May express that someone or something is amazing, fascinating, impressive, or exciting."
+  },
+  {
+    id: 6,
+    emoji: "🤑",
+    name: "Money - Mouth Face",
+    meaning:
+      "A yellow face with raised eyebrows, dollar signs for eyes, and an open smile sticking out a tongue styled after a green, dollar banknote. Some platforms depict the dollar-sign eyes in green circles."
+  } 
+];
+
+export default emojipedia;
